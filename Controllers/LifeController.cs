a7f1a94924c201ca93dcb28d2d468a0c7178e717
RyeGuy12 <hawkinshodgson.1@buckeyemail.osu.edu>
2017-02-06 17:31:56 -0500

    Created three more controllers

---
+++ Controllers/LifeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc; 
+
+[Route("api/life/quotes")]
+public class LifeController : Controller{
+
+    
+    [HttpPost]        
+    public IActionResult Post([FromBody]string value)
+    {           
+         return Created("", value);        
+    }
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)        
+    {           
+         return Ok("The id is: " + id);
+    }        
+    
+    [HttpPut("{id}")]        
+    public IActionResult Put(int id, [FromBody]string value)        
+    {            
+        return NoContent();        
+    }
+    [HttpDelete("{id}")]        
+    public IActionResult Delete(int id)        
+    {            
+        return Delete(id);        
+    }
+
+}
